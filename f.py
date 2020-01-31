a39cfbc940baa9b57bf776b7a6474c6c5daa225c
iamlordutkarsh <lalbaake456@gmail.com>
2020-01-31 22:45:37 +0530

    ADDED MORE STUFF & FIXED

--- f.py
+++ f.py
@@ -1,26 +1,81 @@
-n=raw_input("enter name of emplyee  ")
-p=raw_input("enter pan number ")
-s=int(input("enter salary "))
-if s>250000:
-    if s<500000:
-        k=s-s*5/100
-        print("Salary After 5% Tax",k)
-if s>500000:
-    if s<1000000:
-        k=s-s*20/100
-        print("Salary After 20% Tax",k)
-else:
-    print(s)
-#n2=input("enter name of emplyee")
-#p2=input("enter pan number")
-#s2=int(input("enter salary"))
-##n3=input("enter name of emplyee")
-#p3=input("enter pan number")
-#s3=int(input("enter salary"))
-#n4=input("enter name of emplyee")
-#p4=input("enter pan number")
-#s4=int(input("enter salary"))
-#n4=input("enter name of emplyee")
-#p4=input("enter pan number")
-#s4=int(input("enter salary"))
+#Using "raw_input" instead of "str" due to pyhton 2.x
 
+#1st Person Info
+n1=raw_input("enter name of emplyee:= ")
+p1=raw_input("enter pan number:= ")
+s1=int(input("enter salary:= "))
+if s1>250000:
+    if s1<500000:
+        k1=s1-s1*5/100
+if s1>500000:
+    if s1<1000000:
+        k1=s1-s1*20/100
+if s1>1000000:
+    k1=s1-s1*30/100
+else:
+    k1=s1
+
+#2nd Person Info
+n2=input("enter name of emplyee")
+p2=input("enter pan number")
+s2=int(input("enter salary"))
+if s1>250000:
+    if s1<500000:
+        k1=s1-s1*5/100
+if s1>500000:
+    if s1<1000000:
+        k1=s1-s1*20/100
+if s1>1000000:
+    k1=s1-s1*30/100
+else:
+    k1=s1
+
+#3rd Person Info
+n2=raw_input("enter name of emplyee  ")
+p2=raw_input("enter pan number ")
+s2=int(input("enter salary "))
+if s2>250000:
+    if s2<500000:
+        k2=s2-s2*5/100
+if s2>500000:
+    if s2<1000000:
+        k2=s2-s2*20/100
+if s2>1000000:
+    k2=s2-s2*30/100
+else:
+    k2=s2
+
+#4th Person Info
+n4=raw_input("enter name of emplyee")
+p4=raw_input("enter pan number")
+s4=int(input("enter salary"))
+if s4>250000:
+    if s4<500000:
+        k4=s4-s4*5/100
+if s4>500000:
+    if s4<1000000:
+        k4=s4-s4*20/100
+if s4>1000000:
+    k4=s4-s4*30/100
+else:
+    k4=s4
+
+#5th Person Info
+n4=raw_input("enter name of emplyee")
+p4=raw_input("enter pan number")
+s4=int(input("enter salary"))
+if s5>250000:
+    if s5<500000:
+        k5=s5-s5*5/100
+if s5>500000:
+    if s5<1000000:
+        k5=s5-s5*20/100
+if s5>1000000:
+    k5=s5-s5*30/100
+else:
+    k5=s5
+
+Pan=("Pan NuMBER",p1,p2,p3,p4)
+Name=("Name",n1,n2.n3,n4)
+for i in range(0,5):
+    print(Pan[i]==Name[i])
