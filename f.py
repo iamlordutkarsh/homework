4e3664d704386ad7299f71e06c372a91450643cc
iamlordutkarsh <lalbaake456@gmail.com>
2020-01-31 22:12:21 +0530

    Revert To If

--- f.py
+++ f.py
@@ -1,16 +1,16 @@
 n=raw_input("enter name of emplyee  ")
 p=raw_input("enter pan number ")
 s=int(input("enter salary "))
-while s>250000:
-    while s<500000:
+if s>250000:
+    if s<500000:
         k=s-s*5/100
         print("Salary After 5% Tax",k)
-while s>500000:
-    while s<1000000:
+if s>500000:
+    if s<1000000:
         k=s-s*20/100
         print("Salary After 20% Tax",k)
-print()
-print(s)
+else:
+    print(s)
 #n2=input("enter name of emplyee")
 #p2=input("enter pan number")
 #s2=int(input("enter salary"))
