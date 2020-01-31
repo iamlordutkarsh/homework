89d006c8154c65b099b27764d0d95ace68506397
iamlordutkarsh <lalbaake456@gmail.com>
2020-01-31 21:37:25 +0530

    Added Errors

---
+++ f.py
@@ -0,0 +1,22 @@
+n=str(input("enter name of emplyee  "))
+p=input("enter pan number ")
+s=int(input("enter salary "))
+while s>250000:
+    if s<500000:
+        k=s*5/100
+        print("TEST",k)
+    else :
+    print(s)
+#n2=input("enter name of emplyee")
+#p2=input("enter pan number")
+#s2=int(input("enter salary"))
+##n3=input("enter name of emplyee")
+#p3=input("enter pan number")
+#s3=int(input("enter salary"))
+#n4=input("enter name of emplyee")
+#p4=input("enter pan number")
+#s4=int(input("enter salary"))
+#n4=input("enter name of emplyee")
+#p4=input("enter pan number")
+#s4=int(input("enter salary"))
+
