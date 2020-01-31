1d3c5e258d2e25cfa6a21e8c28ca05eacb62266b
iamlordutkarsh <lalbaake456@gmail.com>
2020-01-31 21:30:32 +0530

    Fix Test

---
+++ file1.py
@@ -0,0 +1,19 @@
+n1=str(input("enter name of emplyee"))
+p1=input("enter pan number")
+s1=int(input("enter salary"))
+while s1>250000:
+    k=s1*5/100
+    print("TEST",k)
+n2=input("enter name of emplyee")
+p2=input("enter pan number")
+s2=int(input("enter salary"))
+n3=input("enter name of emplyee")
+p3=input("enter pan number")
+s3=int(input("enter salary"))
+n4=input("enter name of emplyee")
+p4=input("enter pan number")
+s4=int(input("enter salary"))
+n4=input("enter name of emplyee")
+p4=input("enter pan number")
+s4=int(input("enter salary"))
+
