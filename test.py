822629474bae5cb7dba59f071e2a37475b8853be
iamlordutkarsh <lalbaake456@gmail.com>
2020-02-01 23:21:03 +0530

    f

--- test.py
+++ test.py
@@ -1,36 +1,47 @@
 n1=str(input("Enter User Name :- "))
 p1=input("Enter Pan Number")
 s1=int(input("Enter Salary"))
+tax1=0
+sur1=0
+cess1=0
+total1=0
+
 if s1<250000:
     tax1=0
     sur1=0
     cess1=0
+    total1=0
 
 elif s1>250000:
     if s1<500000:
-        tax1=0.05*s1
-        cess1=0.04*tax1
+        tax1=0.05*(s1)
+        cess1=0.04*(tax1)
         sur1=0
+        total1=tax1 + cess1
 
-elif (s1>500000) or (s1<1000000):
-     tax1=20/100*s1
-     cess1=4/100*(tax1)
-     sur1=0
+elif (s1>500000) or (s1<100000):
+    tax1= 20/100*s1
+    cess1= 4/100*(tax1)
+    sur1=0
+    total1= tax1 + cess1
 
 elif (s1>1000000) or (s1<5000000):
-     tax1=30/100*s1
-     cess1=4/100*(tax1)
-     sur1=0
+    tax1=30/100*s1
+    cess1=4/100*(tax1)
+    sur1=0
+    total1=tax1 + cess1
 
 elif (s1>5000000) or (s1<10000000):
     tax1=30/100*s1
     sur1=10/100*tax1
     cess1=4/100*(tax1 + sur1)
+    total1=tax1 + cess1 + sur1
 
 elif (s1>10000000):
     tax1=30/100*s1
     sur1=15/100*tax1
     cess1=4/100*(tax1 + sur1)
+    total1=tax1 + cess1 + sur1
 
 print(tax1)
 
